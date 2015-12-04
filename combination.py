8669fc8487ab9728bf09b7362169e13d91d62ecc
HannahWang <b00901023@ntu.edu.tw>
2015-12-04 12:31:39 +0800

    complete combination 990
    as title

--- combination.py
+++ combination.py
@@ -1,9 +1,27 @@
-def combination(n, r):
-	if n == r:
-		return 1
-	elif n > 1 and r > 1:
-		return combination(n-1, r) + combination(n-1, r-1)
-	elif r == 1:
-		return n
+class Comb:
+	Cmap = []
+	def combination(self, n, r):
+		n_index = n-1
+		r_index = r-1
+		if not self.Cmap:
+			tmp = []
+			self.Cmap = [tmp]*(n)
+			for i in xrange(n):
+				tmp = []
+				if i < r:
+					self.Cmap[i] = [tmp]*(i+1)
+				else:
+					self.Cmap[i] = [tmp]*(r)
+		if self.Cmap[n_index][r_index]:
+			return self.Cmap[n_index][r_index]
+		elif n == r:
+			return 1
+		elif n > 1 and r > 1:
+			self.Cmap[n_index-1][r_index] = self.combination(n-1, r)
+			self.Cmap[n_index-1][r_index-1] = self.combination(n-1, r-1)
+			return self.Cmap[n_index-1][r_index] + self.Cmap[n_index-1][r_index-1]
+		elif r == 1:
+			return n
 
-print( combination(990,33) )
+Cex = Comb()
+print( Cex.combination(990,33) )
